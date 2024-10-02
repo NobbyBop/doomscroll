40269fe04033c0706dd4462ed68d6387ee8e8052
Nicholas Mirigliani <36427716+NobbyBop@users.noreply.github.com>
2024-10-01 20:53:43 -0400

    small additions

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def redirect_home():
+    return redirect('/home')
+
+@app.route('/login', methods=["GET"])
+def login():
+    return render_template("login.html")
+
+@app.route('/home', methods=["GET", "POST"])
+def home():
     return render_template("home.html")
 
 if __name__ == "__main__":
